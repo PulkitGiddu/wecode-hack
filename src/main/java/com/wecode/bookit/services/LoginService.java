838fd385074d34a6ad2e75f0c7998bbf9ef507ca
PulkitGiddu <pulkitgiddu09@gmail.com>
2025-12-28 23:35:53 +0530

    Remove Unwanted files

--- src/main/java/com/wecode/bookit/services/LoginService.java
+++
@@ -1,8 +0,0 @@
-//package com.wecode.bookit.services;
-//
-//import com.wecode.bookit.model.Users;
-//
-//public interface LoginService {
-//    public Users login(String username, String password);
-//    public void resetCredits(Users user);
-//}
