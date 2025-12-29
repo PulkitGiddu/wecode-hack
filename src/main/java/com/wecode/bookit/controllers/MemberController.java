c133636454e5fe75550d15845b3e38e95ca4d0f3
PulkitGiddu <pulkitgiddu09@gmail.com>
2025-12-30 05:00:34 +0530

    created MemberController

---
+++ src/main/java/com/wecode/bookit/controllers/MemberController.java
@@ -0,0 +1,11 @@
+package com.wecode.bookit.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth/member")
+@CrossOrigin(origins = "*")
+public class MemberController {
+}
